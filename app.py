6402b1ecd47abde0d8063c19e949cfcc4a8bd3b2
Pierre-LouisBJT <78322686+Pierre-LouisBJT@users.noreply.github.com>
2023-09-07 16:43:45 +0200

    Minor change

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from phospho import Agent, Message
 # Initialize the agent
 agent = Agent()
 
-# Define your routes
-############
-#
+# Define your routes #
+
 @agent.ask()
 def myask(message):
     print(f"Your message was {message.content}, now I can do stuff in the background")
