010229dd1252c2c0a1faf2bcaa3efa8cae0860c1
Pierre-LouisBJT <78322686+Pierre-LouisBJT@users.noreply.github.com>
2023-09-09 15:51:22 +0200

    PHB test

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from phospho import Agent, Message
 # Initialize the agent
 agent = Agent()
 
+def ph_agent(input : str):
+    response = f"PH asked : {input}"
+    return response
+
 # Define your routes
 # you don't have to implement both routes
-
+"""
 @agent.ask()
 def myask(message):
     print(f"Your message was {message.content}, now I can do stuff in the background")
+"""
 
 @agent.chat()
 def mychat(message):
-    return Message(f"Hello {message.content}!", {}, {})
+    response = ph_agent(message.content)
+    return Message(response)
 
 # That's it, you're done!
